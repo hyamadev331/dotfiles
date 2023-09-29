e5948a22d64354cd3bedd3e38684d7e8a72dec8d
hyamadev331 <hyamadev331@gmail.com>
2023-09-29 05:37:30 +0000

    feat: Add Zim

--- setup.sh
+++ setup.sh
@@ -11,20 +11,21 @@ sudo apt install --no-install-recommends -y \
     vim \
     zsh \
 
-# setup bash
-#ln -sf $DOTFILE_HOME/bash/.bashrc ~/.bashrc
-#source ~/.bashrc
-
-#setup zsh
-ln -sf $DOTFILE_HOME/zsh/.zshrc ~/.zshrc
-chsh -s `which zsh` $USER 
-source ~/.zshrc
-
 # setup Git
 ln -sf $DOTFILE_HOME/git/.gitconfig ~/.gitconfig
 ln -sf $DOTFILE_HOME/git/.commit_messages ~/.commit_messages
 
 # setup vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -sf $DOTFILE_HOME/vim/.vimrc ~/.vimrc
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#ln -sf $DOTFILE_HOME/vim/.vimrc ~/.vimrc
+
+# setup bash
+#ln -sf $DOTFILE_HOME/bash/.bashrc ~/.bashrc
+#source ~/.bashrc
+
+# setup zsh and Zim
+ln -sf $DOTFILE_HOME/zsh/.zshrc ~/.zshrc
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+ln -sf $DOTFILE_HOME/zsh/.zimrc ~/.zimrc
+exec zsh -l
